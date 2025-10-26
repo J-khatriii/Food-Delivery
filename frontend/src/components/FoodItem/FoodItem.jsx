1f9059389ec8cd4159c2289b2935cabbfb6fc7ea
[Jaswant Khatri] <[jaswantkhatri1440@gmail.com]>
2025-10-26 18:31:27 +0530

    change

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -10,14 +10,19 @@ const FoodItem = ({id,name,price,description,image}) => {
 
     const {cartItems,addToCart,removeFromCart,url} = useContext(StoreContext);
 
+    // Add null check for cartItems
+    // const itemCount = cartItems && cartItems[id] ? cartItems[id] : 0;
+
   return (
     <div className='food-item'>
       <div className="food-item-image-container">
-        {/* <img className='food-item-image' src={url+"/images/"+image} alt="" /> */}
-        <img className='food-item-image' src={`/images/${image}.png`} alt="" />
+
+        <img className='food-item-image' src={url+"/images/"+image} alt="" />
+
+        {/* <img className='food-item-image' src={`/images/${image}.png`} alt="" /> */}
+
         {!cartItems[id]
             ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt=""/>
-            // ?<img className='add' onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt=""/>
             :<div className='food-item-counter'>
                 <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
                 <p>{cartItems[id]}</p>
